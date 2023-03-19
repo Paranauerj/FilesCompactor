33231b701ecbb69144c74a8ad1a44d8fe2bebd1a
Hugo_Andrade <hugobragaandrade@gmail.com>
2023-03-19 02:35:23 +0000

    Adicionado o código para rodar o rsa no Program.cs

--- Program.cs
+++ Program.cs
@@ -15,3 +15,12 @@ Console.WriteLine("Texto decriptado: " + decryptedPlainText);
 
 SHAEncryption shaenc = new SHAEncryption();
 Console.WriteLine("Oi encriptado em sha256: " + shaenc.encrypt(decryptedPlainText));
+
+Console.WriteLine("### RSA Encryption ###");
+Console.WriteLine("Frase Original: " + plainText);
+RSA_Encryption rsa = new RSA_Encryption();
+RSAParameters publicKey, privateKey;
+rsa.GenerateKeys(out publicKey, out privateKey);
+string rsaEncripted = rsa.encrypt_b(plainText, publicKey);
+Console.WriteLine("Encripted: " + rsaEncripted);
+Console.WriteLine("Decrypted: " + rsa.decrypt_b(rsaEncripted, privateKey));
